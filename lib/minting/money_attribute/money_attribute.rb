3e2fb1629503f1ce89df7f61a5d97db5f5318869
Gilson Ferraz <gilson@cesar.etc.br>
2024-04-30 00:15:24 -0300

    Small refactor

--- lib/minting/money_attribute/money_attribute.rb
+++ lib/minting/money_attribute/money_attribute.rb
@@ -8,12 +8,12 @@ module Mint
     class_methods do
       # Money attribute
       def money_attribute(name, currency: 'GBP', mapping: nil)
-        parser = proc { |amount, code = currency| parse_money(amount, code) }
+        parser = proc { |amount, code = currency| MoneyAttribute.parse_money(amount, code) }
         if attribute_names.include? name.to_s
           attribute(name, :mint_money, currency:)
           normalizes(name, with: parser)
         else
-          aggregated = find_money_attributes(name, mapping:)
+          aggregated = MoneyAttribute.find_money_attributes(name, mapping:)
           options = {
             allow_nil: true, class_name: 'Mint::Money',
             constructor: parser, converter: parser,
@@ -22,38 +22,39 @@ module Mint
           composed_of(name, options)
         end
       end
+    end
 
-      private
-
-      def find_money_attributes(name, mapping:)
-        {}.tap do |c|
-          if mapping.present?
-            c[:amount] = mapping.key(:amount).to_s
-            c[:currency] = mapping.key(:currency).to_s
-          else
-            c[:amount] = "#{name}_amount"
-            c[:currency] = "#{name}_currency"
-          end
-          if (c.values & attribute_names).size != 2
-            raise ArgumentError, "Could not find attributes to map to #{name} money attribute"
-          end
-        end
-      end
-
-      def parse_money(amount, currency)
-        case amount
-        when NilClass
-          nil
-        when Mint::Money
-          amount
-        when Numeric
-          Mint.money(amount, currency)
+    def self.find_money_attributes(name, mapping:)
+      {}.tap do |c|
+        if mapping.present?
+          c[:amount] = mapping.key(:amount).to_s
+          c[:currency] = mapping.key(:currency).to_s
         else
-          Mint.money(amount.to_s.split[0].to_r, currency)
+          c[:amount] = "#{name}_amount"
+          c[:currency] = "#{name}_currency"
         end
-        # puts "parse(#{amount}, #{currency}) => #{money.inspect}"
+        if (c.values & attribute_names).size != 2
+          raise ArgumentError, "Could not find attributes to map to #{name} money attribute"
+        end
       end
     end
+
+    def self.parse_money(amount, currency)
+      case amount
+      when NilClass
+        nil
+      when Mint::Money
+        amount
+      when Numeric
+        Mint.money(amount, currency)
+      else
+        Mint.money(amount.to_s.split[0].to_r, currency)
+      end
+      # puts "parse(#{amount}, #{currency}) => #{money.inspect}"
+    end
+
+    private_class_method :find_money_attributes
+    private_class_method :parse_money
   end
 end
 

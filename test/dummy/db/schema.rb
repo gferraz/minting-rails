41a4316837caa58f5f46e639a36760fa7254c3ae
Gilson Ferraz <gilson@cesar.etc.br>
2026-06-18 11:58:12 -0300

    Created Currency model

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_02_041927) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_17_000000) do
+  create_table "currencies", force: :cascade do |t|
+    t.string "code", null: false
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.integer "subunit", default: 2, null: false
+    t.string "symbol", default: "", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_currencies_on_code", unique: true
+  end
+
   create_table "financial_transactions", force: :cascade do |t|
     t.integer "amount"
     t.datetime "created_at", null: false
